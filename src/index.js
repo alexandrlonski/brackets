30c46f9fcda775e71c555c1289235d78f9fee649
Alexandr Lonski <aleksandr.lonski.86@gmail.com>
2021-03-07 18:44:31 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketPairs = bracketsConfig.map(function (val) {
+        return val.join("");
+    });
+
+    let replStr = str;
+    let newStr;
+    do{
+        newStr = replStr;
+        bracketPairs.forEach(function (val) {
+            replStr = replStr.replace(val,"");
+        });
+    } while (replStr.length < newStr.length);
+
+    return replStr.length === 0;
 }
